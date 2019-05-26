68368b7a4c50258b981ac9e2000330a0325c9ceb
CHENQI\cq <448764699@qq.com>
2019-05-26 22:15:40 +0800

    上传到指定的存储位置 云开发：插入数据 和 查询数据

--- miniprogram/pages/publish/publish.js
+++ miniprogram/pages/publish/publish.js
@@ -1,4 +1,8 @@
 // pages/publish/publish.js
+const testDB = wx.cloud.database({
+  env: 'test-52nlc'
+})
+const pic = testDB.collection('pic')
 Page({
 
   /**
@@ -77,6 +81,8 @@ Page({
         console.log("上传的图片路径:" + tempFilePaths[0])
         wx.cloud.uploadFile({
           cloudPath: 'qq.jpg', // 上传至云端的路径
+          config:{ // 上传到指定的存储位置
+            env:"test-52nlc"},
           filePath: tempFilePaths[0], // 小程序临时文件路径
           success: res => {
             // 返回文件 ID
@@ -93,9 +99,19 @@ Page({
                   console.log(res.fileList[0])
                   var tempFileURL = res.fileList[0].tempFileURL;
                   console.log("temp file utl:" + tempFileURL )
+                  pic.add({
+                    data: {
+                      strPath: tempFileURL
+                    },
 
-                  that.setData({
-                    strPublishAddPic: tempFileURL + ' ',
+                    success(res) {
+                      console.log("onPublishAddPic 上传成功！")
+                      console.log(res)
+                    },
+                    fail(err) {
+                      console.log("onPublishAddPic 上传失败！")
+                      console.log(err)
+                    }
                   })
                 }
               },
@@ -108,6 +124,43 @@ Page({
         })
       }
     })
-  }
+  },
+
+  onInsertTestPic:function(){
+    pic.add({
+      data:{
+        strPath:"qq"
+      },
+
+      success(res){
+        console.log(res)
+      },
+      fail(err){
+        console.log(err)
+      }
+    })
+  },
+
+  onGetTestPic: function () {
+    var that = this
+    pic.where({
+      _openid: "oG0Eo46Lf6j-nXHkmqMckGshfn7"
+    }).get({
+      success(res) {
+        // res.data 包含该记录的数据
+        console.log(res)
+        console.log(res.data)
+        console.log(res.data[0])
+        var exp = (res.data[0]);
+        if (typeof (exp) == "undefined") {
+          console.log("无图片")
+        }else{
+            that.setData({
+              strPublishAddPic: res.data[0].strPath + ' ',
+            })
+        }
+      }
+    })
+  },
 
 })
