ab37ebf057b231fe6684897699dd23bb3ca1176f
CHENQI\cq <448764699@qq.com>
2019-06-25 22:44:03 +0800

    activity 页面

---
+++ miniprogram/pages/activity/activity.js
@@ -0,0 +1,74 @@
+// pages/activity/activity.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    activityPicPath: "../../images/create-collection.png",
+    activityInfo:"秋名山社 周三18-21"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  onClickAddress:function(){
+      wx.openLocation({
+        latitude: 23.099994,
+        longitude: 113.32452,
+      })
+    }
+})
