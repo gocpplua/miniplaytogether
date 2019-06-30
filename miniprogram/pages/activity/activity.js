9c9c752cd6c3fca8b682de2b6ec47fa50468f18e
CHENQI\cq <448764699@qq.com>
2019-06-30 22:24:49 +0800

    报名成功后插入头像并且显示

--- miniprogram/pages/activity/activity.js
+++ miniprogram/pages/activity/activity.js
@@ -18,13 +18,14 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log('onLoad')
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+    console.log('onReady')
     this.getActivityAvatarUrl("activity")
   },
 
@@ -32,34 +33,35 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    console.log('onShow')
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    console.log('onHide')
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    console.log('onUnload')
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    console.log('onPullDownRefresh')
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    console.log('onReachBottom')
   },
 
   /**
@@ -85,12 +87,19 @@ Page({
   },
 
   bindGetUserInfo:function(e) {
+    console.log('bindGetUserInfo')
     console.log(e.detail.userInfo)
-    var avatarUrlTmp = e.detail.userInfo.avatarUrl
+    var avatarUrlTmp = 
+      {
+        avatarUrl:e.detail.userInfo.avatarUrl
+      }
 
+
+    this.data.avatarUrl.push(avatarUrlTmp)
+    console.log(this.data.avatarUrl)
     this.setData(
       {
-        avatarUrl:avatarUrlTmp
+        avatarUrl: this.data.avatarUrl
       }
     )
 
@@ -98,7 +107,7 @@ Page({
     todos.add({
       // data 字段表示需新增的 JSON 数据
       data: {
-        avatarUrl: avatarUrlTmp
+        avatarUrl: e.detail.userInfo.avatarUrl
       }
     }).then(res => {
       console.log(res)
