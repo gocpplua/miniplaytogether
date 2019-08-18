ac8f09528ccf58bdb09af0d1598cc98c3cb02039
CHENQI\cq <448764699@qq.com>
2019-08-18 09:22:07 +0800

    当用户报名信息为空的时候，云函数崩溃问题

--- cloudfunctions/getAllUserSignUpInfo/index.js
+++ cloudfunctions/getAllUserSignUpInfo/index.js
@@ -4,13 +4,15 @@ const cloud = require('wx-server-sdk')
 cloud.init()
 
 const db = cloud.database({
-  //env: 'test-52nlc'
-  env: 'qiumingshan'
+  env: 'test-52nlc'
+  //env: 'qiumingshan'
 })
 const MAX_LIMIT = 20
 // 云函数入口函数
 exports.main = async (event, context) => {
   var myavtivityid = event.avtivityid
+  console.log('server api getAllUserSignUpInfo myavtivityid', myavtivityid)
+
   // 先取出集合记录总数
   const countResult = await db.collection('usersignupdb').count()
   const total = countResult.total
@@ -24,6 +26,16 @@ exports.main = async (event, context) => {
         db_avtivityid: myavtivityid
     }).get()
     tasks.push(promise)
+  }
+
+  let nLen = tasks.length
+  console.log('server api getAllUserSignUpInfo tasks len:', nLen)
+  if (nLen <= 0)
+  {
+    return {
+      data: [],
+      errMsg: 'no one signup activity',
+    }
   }
   // 等待所有
   return (await Promise.all(tasks)).reduce((acc, cur) => {
