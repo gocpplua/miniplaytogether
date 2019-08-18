af57b7add69338c5e297d7d6717d896ffe6ec74d
CHENQI\cq <448764699@qq.com>
2019-08-18 10:29:34 +0800

    报名后，在活动信息表中更新 报名人数 取消报名后，在活动信息表中更新 报名人数

--- cloudfunctions/baomingactivity/index.js
+++ cloudfunctions/baomingactivity/index.js
@@ -1,12 +1,11 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
 
-cloud.init()
-
-const db = cloud.database({
-  //env: 'test-52nlc'
-  env: 'qiumingshan'
+cloud.init({
+  env: 'test-52nlc'
 })
+
+const db = cloud.database()
 
 // 云函数入口函数
 exports.main = async (event, context) => {
@@ -14,17 +13,41 @@ exports.main = async (event, context) => {
   var openid = wxContext.OPENID
   var myavatarUrl = event.avatarUrl
   var myavtivityid =  event.avtivityid
+  console.log('baomingactivity myavtivityid:', myavtivityid)
   try {
-    return await db.collection('usersignupdb').add({
+    let userSignUpDatas = await db.collection('usersignupdb').add({
       // data 字段表示需新增的 JSON 数据
       data: {
         db_avtivityid: myavtivityid,
         db_openid: openid,
         db_avatarUrl: myavatarUrl
+      },
+      success: function (res) {
+        // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
+        console.log('baomingactivity server success', res)
+      },
+      fail:function(err){
+        console.log('baomingactivity server fail', err)
+      },
+      complete:function(res){
+        console.log('baomingactivity server complete', res)
       }
     })
+    console.log('baomingactivity userSignUpDatas', userSignUpDatas)
+
+    const _ = db.command
+    let activitydbActPeople = await db.collection('activitydb').where({
+      db_avtivityid: myavtivityid
+    }).update({
+      data: {
+        db_actpeople: _.inc(1)
+      },
+    })
+    console.log('baomingactivity activitydbActPeople', activitydbActPeople, ' myavtivityid', myavtivityid)
+    return userSignUpDatas
   } catch (e) {
     console.error(e)
+    console.log('baomingactivity server catch err', e)
     return e
   }
-}
+} 
--- cloudfunctions/cancelBaoming/index.js
+++ cloudfunctions/cancelBaoming/index.js
@@ -4,8 +4,8 @@ const cloud = require('wx-server-sdk')
 cloud.init()
 
 const db = cloud.database({
-  //env: 'test-52nlc'
-  env: 'qiumingshan'
+  env: 'test-52nlc'
+  //env: 'qiumingshan'
 })
 
 // 云函数入口函数
@@ -15,11 +15,24 @@ exports.main = async (event, context) => {
   var myavtivityid = event.avtivityid
   // 获取玩家是否已经报名
   try {
-    return await db.collection('usersignupdb').where({
+    let userSignUpDatas = await db.collection('usersignupdb').where({
       db_avtivityid: myavtivityid,
       db_openid: openid,
     }).remove()
+    console.log('cancelBaoming userSignUpDatas', userSignUpDatas)
+
+    const _ = db.command
+    let activitydbActPeople = await db.collection('activitydb').where({
+      db_avtivityid: myavtivityid
+    }).update({
+      data: {
+        db_actpeople: _.inc(-1)
+      },
+    })
+    console.log('cancelBaoming activitydbActPeople', activitydbActPeople, ' myavtivityid', myavtivityid)
+    return userSignUpDatas
   } catch (e) {
     console.error(e)
-  }
+    console.log('cancelBaoming server catch err', e)
+    return e  }
 }
